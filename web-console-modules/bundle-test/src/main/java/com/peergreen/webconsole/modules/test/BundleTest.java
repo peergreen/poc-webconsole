9436f61bc545e839bbe8d152bdb461ff47876027
Mohammed Boukada <mohammed.boukada@peergreen.com>
2013-05-16 16:07:25 +0200

    A bundle for test

---
+++ web-console-modules/bundle-test/src/main/java/com/peergreen/webconsole/modules/test/BundleTest.java
@@ -0,0 +1,40 @@
+package com.peergreen.webconsole.modules.test;
+
+import com.peergreen.webconsole.core.api.IViewContribution;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.Label;
+import org.apache.felix.ipojo.annotations.Instantiate;
+import org.apache.felix.ipojo.annotations.Provides;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mohammed
+ * Date: 14/05/13
+ * Time: 13:43
+ * To change this template use File | Settings | File Templates.
+ */
+@org.apache.felix.ipojo.annotations.Component
+@Provides
+@Instantiate
+public class BundleTest implements IViewContribution {
+    @Override
+    public com.vaadin.ui.Component getView() {
+        CssLayout component = new CssLayout();
+        component.addStyleName("timeline");
+
+        Label header = new Label("This is a view contribution ! ");
+        header.addStyleName("h1");
+        component.addComponent(header);
+        return component;
+    }
+
+    @Override
+    public String getScope() {
+        return "store";
+    }
+
+    @Override
+    public String getName() {
+        return "Store Billing";
+    }
+}
