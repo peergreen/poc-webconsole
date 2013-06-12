1725fce40d576bb0d36cae54938c2626b289de4d
Mohammed Boukada <mohammed.boukada@peergreen.com>
2013-06-12 17:44:13 +0200

    Utils classes

--- web-console-core/src/main/java/com/peergreen/webconsole/core/scope/tabs/DefaultTab.java
+++ web-console-core/src/main/java/com/peergreen/webconsole/core/scope/tabs/DefaultTab.java
@@ -1,6 +1,5 @@
 package com.peergreen.webconsole.core.scope.tabs;
 
-import com.peergreen.webconsole.module.IModuleFactory;
 import com.vaadin.event.LayoutEvents;
 import com.vaadin.server.ThemeResource;
 import com.vaadin.ui.Component;
@@ -9,40 +8,39 @@ import com.vaadin.ui.Image;
 import com.vaadin.ui.TabSheet;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Created with IntelliJ IDEA.
- * User: mohammed
- * Date: 28/05/13
- * Time: 16:35
- * To change this template use File | Settings | File Templates.
+ * Default tab for scope tab view
+ * @author Mohammed Boukada
  */
 public class DefaultTab extends CssLayout {
 
     private TabSheet tabs;
 
-    private HashMap<IModuleFactory, Component> modulesComponents = new HashMap<>();
+    private Map<String, Component> modulesComponents = new HashMap<>();
 
-    private HashMap<IModuleFactory, Component> modulesFrames = new HashMap<>();
+    private Map<String, Component> modulesFrames = new HashMap<>();
 
     public DefaultTab(TabSheet tabs) {
         this.tabs = tabs;
         addStyleName("catalog");
+        setSizeFull();
     }
 
-    public void addModuleButtonInDefaultTab(final IModuleFactory moduleFactory, Component component) {
+    public void addModuleButtonInDefaultTab(final String scopeElementName, Component component) {
         Image moduleIcon = new Image(
-                moduleFactory.getName(),
+                scopeElementName,
                 new ThemeResource("img/default-module-icon.png"));
         CssLayout frame = new CssLayout();
         frame.addComponent(moduleIcon);
         frame.addLayoutClickListener(new LayoutEvents.LayoutClickListener() {
             @Override
             public void layoutClick(LayoutEvents.LayoutClickEvent event) {
-                Component moduleComponent = modulesComponents.get(moduleFactory);
+                Component moduleComponent = modulesComponents.get(scopeElementName);
                 if (tabs.getTab(moduleComponent) == null) {
                     tabs.addComponent(moduleComponent);
-                    tabs.getTab(moduleComponent).setCaption(moduleFactory.getName());
+                    tabs.getTab(moduleComponent).setCaption(scopeElementName);
                     tabs.getTab(moduleComponent).setClosable(true);
                     tabs.setSelectedTab(moduleComponent);
                 } else {
@@ -51,11 +49,11 @@ public class DefaultTab extends CssLayout {
             }
         });
         addComponent(frame);
-        modulesComponents.put(moduleFactory, component);
-        modulesFrames.put(moduleFactory, frame);
+        modulesComponents.put(scopeElementName, component);
+        modulesFrames.put(scopeElementName, frame);
     }
 
-    public void removeModuleButtonFromDefautlTab(IModuleFactory moduleFactory) {
-        removeComponent(modulesFrames.get(moduleFactory));
+    public void removeModuleButtonFromDefautlTab(String scopeElementName) {
+        removeComponent(modulesFrames.get(scopeElementName));
     }
 }
