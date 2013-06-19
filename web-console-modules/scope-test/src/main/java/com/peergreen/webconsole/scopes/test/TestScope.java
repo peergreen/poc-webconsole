837f2eab572d295838cd4fe3d5ca454977477cb9
Mohammed Boukada <mohammed.boukada@peergreen.com>
2013-06-19 11:34:31 +0200

    Add default tab

--- web-console-modules/scope-test/src/main/java/com/peergreen/webconsole/scopes/test/TestScope.java
+++ web-console-modules/scope-test/src/main/java/com/peergreen/webconsole/scopes/test/TestScope.java
@@ -7,7 +7,9 @@ import com.peergreen.webconsole.Inject;
 import com.peergreen.webconsole.Link;
 import com.peergreen.webconsole.Ready;
 import com.peergreen.webconsole.Scope;
+import com.peergreen.webconsole.UIContext;
 import com.peergreen.webconsole.Unlink;
+import com.peergreen.webconsole.tabs.DefaultTab;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.TabSheet;
 
@@ -18,14 +20,20 @@ import java.util.Dictionary;
  */
 @Extension
 @ExtensionPoint("com.peergreen.webconsole.pgadmin.scope")
-@Scope(name = "test")
+@Scope("test")
 public class TestScope extends TabSheet {
 
     @Inject
     INotifierService notifierService;
+    @Inject
+    UIContext uiContext;
+
+    DefaultTab defaultTab = new DefaultTab(this);
 
     @Ready
     public void buildComponent() {
+        defaultTab.setUi(uiContext.getUI());
+        addTab(defaultTab, "Test", null, 0);
         setSizeFull();
         setCloseHandler(new TabSheet.CloseHandler() {
             @Override
@@ -41,10 +49,12 @@ public class TestScope extends TabSheet {
     public void addTabs(Component tab, Dictionary properties) {
         tab.setSizeFull();
         addTab(tab, (String) properties.get("tab.name")).setClosable(true);
+        defaultTab.addExtension(tab, (String) properties.get("tab.name"));
     }
 
     @Unlink("tab")
-    public void removeTabs(Component tab) {
+    public void removeTabs(Component tab, Dictionary properties) {
         removeComponent(tab);
+        defaultTab.removeExtension((String) properties.get("tab.name"));
     }
 }
